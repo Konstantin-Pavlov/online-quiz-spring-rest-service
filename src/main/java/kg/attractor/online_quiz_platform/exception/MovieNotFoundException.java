7d10689d72bd41fac32a4cec1b1b7a5b51336715
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-08-09 01:27:27 +0600

    delete some no longer needed classes

--- src/main/java/kg/attractor/online_quiz_platform/exception/MovieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package kg.attractor.online_quiz_platform.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-    public MovieNotFoundException(String message) {
-        super(message);
-    }
-}
