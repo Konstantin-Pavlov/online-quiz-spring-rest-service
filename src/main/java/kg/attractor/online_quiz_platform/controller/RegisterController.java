eacbe459a38bbd7ad63201ee14a3e1fc140cfab1
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-08-13 12:28:48 +0600

    update

--- src/main/java/kg/attractor/online_quiz_platform/controller/RegisterController.java
+++ src/main/java/kg/attractor/online_quiz_platform/controller/RegisterController.java
@@ -3,26 +3,20 @@ package kg.attractor.online_quiz_platform.controller;
 import jakarta.validation.Valid;
 import kg.attractor.online_quiz_platform.dto.UserDto;
 import kg.attractor.online_quiz_platform.exception.EmailAlreadyExistsException;
-import kg.attractor.online_quiz_platform.exception.UserNotFoundException;
 import kg.attractor.online_quiz_platform.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("api/register")
 @RequiredArgsConstructor
-public class registerController {
+public class RegisterController {
     private final UserService userService;
 
     @PostMapping
